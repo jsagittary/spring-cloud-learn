018283b56e70cf435118c73f6d510884555ed8dd
zhoujie <huangyin22>
2020-05-18 18:31:35 +0800

    pom修改

--- eureka/eureka-server/src/main/java/com/scl/eurekaserver/EurekaServerApplication.java
+++ eureka/eureka-server/src/main/java/com/scl/eurekaserver/EurekaServerApplication.java
@@ -4,12 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @program: spring-cloud-learn
+ * @description:
+ * @author: zhou jie
+ * @create: 2020-05-18 18:27
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run( EurekaServerApplication.class, args );
     }
-
 }
--- eureka/eureka-server/src/test/java/com/scl/eurekaserver/EurekaServerApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.scl.eurekaserver;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaServerApplicationTests {
-
-
-}
